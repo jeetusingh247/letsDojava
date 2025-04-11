607f8fadf46a2a8c7c746c149d48a57034b3a68c
jeetusingh247 <jeetusingh11010@gmail.com>
2025-04-11 22:41:32 +0530

    intro to java

---
+++ firstProject01/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // THIS IS MY FIRST JAVA PROGRAM
+        /*
+         * This 
+         * is 
+         * a 
+         * multiline
+         * comment
+         */
+
+        System.out.println("I like Shahi Paneer !!"); // println for new line
+        System.out.print("It's tasty!\n"); // escape sequence char for new line
+        System.out.print("Made with paneer and gravy");
+    }
+}
