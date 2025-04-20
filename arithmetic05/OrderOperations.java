4bbc9124a4264a98c356455b10975b50b583f3ab
jeetusingh247 <jeetusingh11010@gmail.com>
2025-04-20 22:25:04 +0530

    operator precedence

---
+++ arithmetic05/OrderOperations.java
@@ -0,0 +1,17 @@
+// package arithmetic05;
+
+public class OrderOperations {
+
+    public static void main(String[] args) {
+
+        // ORDER OF OPERATORS [P-E-M-D-A-S] 
+        // TO REMEMBER : Please excuse my dope as swag
+        // Parenthesis --> Exponent --> Multiplication --> Division --> Addition -- > Subtraction
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+
+        System.out.println("Result is : " + result); // 7.0
+
+    }
+    
+}
