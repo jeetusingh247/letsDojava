362977214fa9248ff5f84b45ed20944e425483e0
jeetusingh247 <jeetusingh11010@gmail.com>
2025-04-20 22:24:24 +0530

    arithmetic operators

---
+++ arithmetic05/Main.java
@@ -0,0 +1,56 @@
+// package arithmetic05;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // ARITHMETIC OPERATORS
+
+        // int x = 10;
+        // int y = 2;
+        // int z;
+
+        // z = x + y;
+        // z = x / y;
+        // z = x * y;
+        // z = x - y;
+        // z = x % y; // for remainder
+
+
+        // System.out.println("Value : "  + z);
+
+        // AUGMENTED ASSIGNEMENT OPERATORS
+
+        // int x = 10;
+        // int y = 3;
+
+        // double x = 10;
+        // double y = 3;
+
+        // x = x + y;
+        // x = x - y;
+        // x = x * y;
+        // x = x / y;
+        // x = x % y;
+
+        // x += y;
+        // x -= y;
+        // x *= y;
+        // x /= y;
+        // x %= y;
+
+        // System.out.println("Value : " + x);
+
+        // INCREMENT AND DECREMENT OPERATORS
+
+        int x = 1;
+
+        x++; // 2
+        x--; // 1
+        x--; // 0
+
+        System.out.println("Value : " + x); // 0
+
+    }
+    
+}
