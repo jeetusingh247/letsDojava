dd5fb81e490df03d098559e774ee75e02f2e74b8
jeetusingh247 <jeetusingh11010@gmail.com>
2025-07-18 15:13:35 +0530

    string methods in java

---
+++ string_methods13/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        String name = "";
+        String fullname = "";
+
+        // int length = name.length();
+        // char letter = name.charAt(0);
+        // int index = name.indexOf("R"); // 10, give first index
+        // int lastIdx = name.lastIndexOf("a"); // 11, last index
+
+        // name = name.toLowerCase();
+        // name = name.toUpperCase();
+        // name = name.trim();
+
+
+        // System.out.println("Saare Bolo " + name);
+        // System.out.printf("Word Length : %d\n", length);
+        // System.out.println("Index of word R : " + index);
+
+        // if (name.isEmpty()) {
+        //     System.out.println("Bhakt likho Jai Shree Ram");
+        // } else{
+        //     System.out.println(name.trim());
+        // }
+
+        // if (name.contains(" ")) {
+        //     System.out.println("Space Exist");
+        // }else{
+        //     System.out.println("Space does'nt exist");
+        // }
+
+        if (name.equals(fullname)) {
+            System.out.println("name is equal to fullname");
+        } else {
+            System.out.println("Name is not equal to fullname");
+        }
+    }
+}
