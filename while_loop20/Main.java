9b3c51420821c47b2f293eb9a955f5e4e71228f2
jeetusingh247 <jeetusingh11010@gmail.com>
2025-08-18 19:23:46 +0530

    added do-while

--- while_loop20/Main.java
+++ while_loop20/Main.java
@@ -16,15 +16,29 @@ public class Main {
 
         // System.out.println("Hello " + name);
 
-        String response = "";
+        // String response = "";
 
-        while (!response.equals("Q")) {
-            System.out.println("You are playing a game");
-            System.out.print("Press Q to quit : ");
-            response = in.next().toUpperCase();
-        }
+        // while (!response.equals("Q")) {
+        //     System.out.println("You are playing a game");
+        //     System.out.print("Press Q to quit : ");
+        //     response = in.next().toUpperCase();
+        // }
 
-        System.out.println("You quit the game");
+        // System.out.println("You quit the game");
+
+        // Example do while
+
+        int age;
+
+        do {
+            System.out.print("Enter your age : ");
+            age = in.nextInt();
+            if (age >= 18 && age <=100) {
+                System.out.println("You are eligible to vote");
+            }else{
+                System.out.println("You are not eligible to vote");
+            }
+        }while(age != 0);
 
         in.close();
     }
